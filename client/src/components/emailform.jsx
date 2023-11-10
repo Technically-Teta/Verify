a4647907f303482b9883e4e6da56fc8713da9888
Sameliah Quioh <sameliahquioh@Sameliahs-iMac.local>
2023-11-09 20:15:11 -0800

    adds env variables to email form

--- client/src/components/emailform.jsx
+++ client/src/components/emailform.jsx
@@ -1,5 +1,5 @@
 import emailjs from 'emailjs-com';
-import {useState} from 'react'
+import React, { useState } from 'react';
 
 function EmailForm() {
 
@@ -7,6 +7,10 @@ function EmailForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+
+
+const userKey= process.env.REACT_APP_PUBLICKEY
+const templateKey= process.env.REACT_APP_TEMPLATE
 
  // function to send email on submission
  const handleSubmit = (e) => {
@@ -27,7 +31,7 @@ function EmailForm() {
     
 // API functions
   emailjs 
-    .send(data.publicKey, data.templateId, template)
+    .send(userKey, templateKey, template)
     .then(
         function(response) {
             console.log("sent successfully", response.status,response.text);
@@ -38,10 +42,6 @@ function EmailForm() {
 
     );
       
-
-
-
-
 
   return (
     <form onSubmit={handleSubmit} className='emailform'>
