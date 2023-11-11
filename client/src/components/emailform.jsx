4ebe1197afa4e73fee2ec85fbe25150f04febb10
Sameliah Quioh <sameliahquioh@Sameliahs-iMac.local>
2023-11-10 22:43:55 -0800

    adds jest testing package

--- client/src/components/emailform.jsx
+++ client/src/components/emailform.jsx
@@ -36,7 +36,7 @@ const template = {
 
 // API functions
   emailjs 
-    .send(serviceID, templateID, template)
+    .send(serviceID, templateID, userID, template)
     .then(
         function(response) {
             console.log("sent successfully", response.status,response.text);
